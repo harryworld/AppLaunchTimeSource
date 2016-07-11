0726141df8320b0b42ce5589e8c72e4292474bf3
Harry Ng <harryworld@gmail.com>
2016-07-11 23:26:31 +0800

    Import source codes directly

--- AppLaunchTimeSource/ViewController.swift
+++ AppLaunchTimeSource/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSDate().add(1.days)
     }
 
     override func didReceiveMemoryWarning() {
